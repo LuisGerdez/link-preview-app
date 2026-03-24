0b8f7c8922c1e6bccdd3b2d8a8d41a7263245611
Luis Gerdez <elcreidoluis@gmail.com>
2026-03-23 22:59:02 -0500

    add MetaTags type definition for Open Graph and Twitter metadata

---
+++ app/types/meta.ts
@@ -0,0 +1,14 @@
+export type MetaTags = {
+  og_title: string | null;
+  og_description: string | null;
+  og_image: string | null;
+  og_url: string | null;
+
+  twitter_card: string | null;
+  twitter_title: string | null;
+  twitter_description: string | null;
+  twitter_image: string | null;
+  
+  html_title: string | null;
+  meta_description: string | null;
+};
